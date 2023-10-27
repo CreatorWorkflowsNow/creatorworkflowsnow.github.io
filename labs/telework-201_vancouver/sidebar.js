b5a9ad367993376aba6d1877352ac678905fb8bc
MarcMouries <Marc@Mouries.net>
2023-10-27 14:46:58 -0400

    + navigation 201

--- labs/telework-201_vancouver/sidebar.js
+++ labs/telework-201_vancouver/sidebar.js
@@ -6,12 +6,52 @@ const sidebars = {
         
         {
             type: 'category',
-            label: 'Telework Case Management 201',
+            label: 'Overview',
             items: [
-                '0.0-introduction/Overview'
+                'Introduction/Overview'
             ],
         },
 
+        {
+            type: 'category',
+            label: '1. Data',
+            items: [
+                'Data/Overview',
+            ],
+        },
+
+        
+        {
+            type: 'category',
+            label: '2-Security',
+            items: [
+                'Security/Overview',
+            ],
+        },
+
+        {
+            type: 'category',
+            label: '3-Portal',
+            items: [
+                'Portal/Overview',
+            ],
+        },
+
+        {
+            type: 'category',
+            label: '4-Flow',
+            items: [
+                'Flow/Overview',
+            ],
+        },
+
+        {
+            type: 'category',
+            label: '5-Integration',
+            items: [
+                'Integration/Overview',
+            ],
+        },
     ],
 };
 
