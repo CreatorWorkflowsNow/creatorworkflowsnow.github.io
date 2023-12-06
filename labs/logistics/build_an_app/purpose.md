c9579936a8d67d1fee538ce37e410b0358c9cf35
DaleNow <dalestubblefield@me.com>
2023-12-06 17:43:15 -0600

    Added all files without content
    Initial file structure and sidebar created. Files need screenshots and rewrite of instructions.

--- labs/logistics/build_an_app/purpose.md
+++
@@ -1,8 +0,0 @@
----
-id: purpose
-sidebar_position: 4
-sidebar_label: Purpose
-title: Purpose
-hide_table_of_contents: true
-draft: true
----
--- labs/logistics/sidebar.js
+++ labs/logistics/sidebar.js
@@ -2,23 +2,85 @@
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-    logistics: {
+    logistics: [
 
-        'Logistics Lab': [
-            'get_started/overview',
-            'get_started/goals',
-            'get_started/usecase',
-            'get_started/setup',
-        ],
+        'vancouver',
 
-        '1. Build an App': [
-            'build_an_app/overview',
-            'build_an_app/section1',
-            'build_an_app/section2',
-            'build_an_app/purpose',
-        ],
+        {
+            type: 'category',
+            label: 'Logistics Lab',
+            items: [
+                '0.0-overview',
+                '0.1-goals',
+                '0.2-usecase',
+                '0.3-setup',
+            ],
+        },
 
-    },
+        {
+            type: 'category',
+            label: '1. Build an App',
+            items: [
+                '1.0-overview',
+                '1.1-create-app',
+                '1.2-create-data',
+                '1.3-review',
+            ],
+        },
+
+        {
+            type: 'category',
+            label: '2. Create Workspace',
+            items: [
+                '2.0-overview',
+                '2.1-create-workspace',
+                '2.2-configure-workspace',
+                '2.3-test-workspace',
+                '2.4-review',
+            ],
+        },
+
+        {
+            type: 'category',
+            label: '3. Add Automation',
+            items: [
+                '3.0-overview',
+                '3.1-add-a-flow',
+                '3.2-test-the-flow',
+                '3.3-review',
+            ],
+        },
+
+        {
+            type: 'category',
+            label: '4. Mobile',
+            items: [
+                '4.0-overview',
+                '4.1-create-mobile',
+                '4.2-review',
+            ],
+        },
+
+        {
+            type: 'category',
+            label: 'Conclusion',
+            items: [
+                '5.0-lab-review',
+                '5.1-supplemental-resources',
+            ],
+        },
+
+        'lab-guide-pdf',
+
+        {
+            type: 'link',
+            label: 'Post Workshop Survey', 
+            href: 'https://forms.office.com/r/KcNQR6ApJ3',
+        },
+
+    ],
+
+
 };
 
 module.exports = sidebars;
